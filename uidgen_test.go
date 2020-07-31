86be1cceafb3cbcda1e8c765b02fd326fc0ad956
Ahmad Satryaji Aulia <satryaji.aulia@ovo.id>
2020-07-31 10:56:47 +0700

    Implement uniqueness test

--- uidgen_test.go
+++ uidgen_test.go
@@ -1,6 +1,7 @@
 package uidgen
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -8,11 +9,21 @@ import (
 func TestUIDGen(t *testing.T) {
 	guid := New(0, VARCHAR26)
 	uid := guid.UID()
+
 	p, ok := guid.Parse(uid.String())
 	if !ok {
 		t.Errorf("Generated UID %s failed to parse", uid)
 	}
 	if !reflect.DeepEqual(uid, p) {
 		t.Errorf("Generated UID %s not equal with parse", uid)
+	}
+
+	exists := make(map[string]bool)
+	for i := 0; i < int(math.Pow10(6)); i++ {
+		uid := guid.UID().String()
+		if exists[uid] {
+			t.Errorf("Generated UID %s clashes at iteration %d", uid, i+1)
+		}
+		exists[uid] = true
 	}
 }
